05edacaac71cf16ebe921986834bc9ac986d3add
ccosta_ricardo <ricardonunocamposcosta@gmail.com>
2025-07-08 17:21:20 +0100

    Teste para o render funcionar

--- app.py
+++ app.py
@@ -8,37 +8,39 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], use_pages
 server = app.server
 
 # Importar páginas después de inicializar la app
-from pages import cargar_datos, sessionReport, settings, summary
+#from pages import cargar_datos, sessionReport, settings, summary
+
+app.layout = html.Div("Página de teste")
 
 # Layout principal con sidebar y área de contenido
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div([
-        make_sidebar(),
-    ], style={"width": "15%", "float": "left", "height": "100vh"}),
-    html.Div([
-        html.Div(id='page-content')
-    ], style={"width": "85%", "float": "right", "padding": "3rem"})
-])
+# app.layout = html.Div([
+#     dcc.Location(id='url', refresh=False),
+#     html.Div([
+#         make_sidebar(),
+#     ], style={"width": "15%", "float": "left", "height": "100vh"}),
+#     html.Div([
+#         html.Div(id='page-content')
+#     ], style={"width": "85%", "float": "right", "padding": "3rem"})
+# ])
 
-# Callback para renderizar la página correcta
-@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
-def display_page(pathname):
-    # Esta función selecciona el layout de la página según el URL
-    if pathname == '/cargar_datos':
-        return cargar_datos.layout
-    elif pathname == '/sessionReport':
-        return sessionReport.layout
-    elif pathname == '/settings':
-        return settings.layout
-    elif pathname == '/summary':
-        return summary.layout
-    else:
-        return html.H1('Bienvenido a Performance APP')
+# # Callback para renderizar la página correcta
+# @app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+# def display_page(pathname):
+#     # Esta función selecciona el layout de la página según el URL
+#     if pathname == '/cargar_datos':
+#         return cargar_datos.layout
+#     elif pathname == '/sessionReport':
+#         return sessionReport.layout
+#     elif pathname == '/settings':
+#         return settings.layout
+#     elif pathname == '/summary':
+#         return summary.layout
+#     else:
+#         return html.H1('Bienvenido a Performance APP')
 
 # Registrar callbacks das páginas
-cargar_datos.register_callbacks(app)
-sessionReport.register_callbacks(app)
+# cargar_datos.register_callbacks(app)
+# sessionReport.register_callbacks(app)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
